e6b319ee28aec069a5ad55cda98e1f415d46fd6a
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-04-29 14:06:59 +0530

    Tweaked settings

--- problem3/settings.py
+++ problem3/settings.py
@@ -23,7 +23,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -34,7 +33,7 @@ ROOT_URLCONF = 'problem3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
