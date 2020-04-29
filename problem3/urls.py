fe82493ad70ec32a275179b3123f0d0ecb8de64f
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-04-29 14:55:29 +0530

    Tweaked settings

--- problem3/urls.py
+++ problem3/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from . import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('project.urls')),
+    path('', include('project.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_url=settings.STATIC_ROOT)
